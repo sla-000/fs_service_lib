d7bda75a5a4a9108bbe8d4031af6cd294919c661
Slava Ryabinin <ryabinin.v.v@gmail.com>
2025-06-29 15:54:40 +0400

    Check updateDocument feature

--- example/fs_service_lib_example.dart
+++ example/fs_service_lib_example.dart
@@ -12,38 +12,59 @@ void main() async {
     // databaseId: 'databaseId',
   );
 
-  await fsServiceLib.db.addDocument(
-    collectionPath: '/test',
-    json: {
-      'textField': 'value1', // a String field
-      'doubleField': 1234.5432, // a floating point number field
-      'integerField': 8765, // an integer number field
-      'geopoint': 'location://34.3456/-23.432', // a location point field
-      'timestamp': 'datetime://2023-10-21T11:26:40.152Z', // a timestamp field
-      'docReference':
-          'reference://projects/ella500/databases/(default)/documents/'
-              'test/test-34f22dd23', // a reference field
-      'blobData': 'bytes://AAECAwQFBg==', // a binary data field
-      r'$name': 'test-1000', // name of the document
-    },
-  );
-
-  await fsServiceLib.db.addDocument(
-    collectionPath: '/test',
-    json: {
-      'textField': 'value2', // a String field
-      r'$name': 'test-2000', // name of the document
-    },
-  );
+  // await fsServiceLib.db.addDocument(
+  //   collectionPath: '/test',
+  //   json: {
+  //     'textField': 'value1', // a String field
+  //     'doubleField': 1234.5432, // a floating point number field
+  //     'integerField': 8765, // an integer number field
+  //     'geopoint': 'location://34.3456/-23.432', // a location point field
+  //     'timestamp': 'datetime://2023-10-21T11:26:40.152Z', // a timestamp field
+  //     'docReference':
+  //         'reference://projects/ella500/databases/(default)/documents/'
+  //             'test/test-34f22dd23', // a reference field
+  //     'blobData': 'bytes://AAECAwQFBg==', // a binary data field
+  //     r'$name': 'test-1000', // name of the document
+  //   },
+  // );
+  //
+  // await fsServiceLib.db.addDocument(
+  //   collectionPath: '/test',
+  //   json: {
+  //     'textField': 'value2', // a String field
+  //     r'$name': 'test-2000', // name of the document
+  //   },
+  // );
 
   final test1000Doc =
       await fsServiceLib.db.getDocument(documentPath: '/test/test-1000');
-
   stdout.writeln('test1000Doc=`$test1000Doc`'); // test1000Doc=`{
   // textField: value1, integerField: 8765, blobData: bytes://AAECAwQFBg==,
   // docReference: reference://projects/ella500/databases/(default)/documents/test/test-34f22dd23,
   // timestamp: datetime://2023-10-21T11:26:40.152Z, geopoint: location://34.3456/-23.432,
-  // doubleField: 1234.5432, $name: test-1000, $createTime: 2025-06-29T11:39:30.294544Z,
-  // $updateTime: 2025-06-29T11:39:30.294544Z
+  // doubleField: 1234.5432,
+  // $name: test-1000,
+  // $createTime: 2025-06-29T11:39:30.294544Z, $updateTime: 2025-06-29T11:39:30.294544Z
+  // }`
+
+  var test2000Doc =
+      await fsServiceLib.db.getDocument(documentPath: '/test/test-2000');
+  stdout.writeln('test2000Doc=`$test2000Doc`'); // test2000Doc=`{
+  // textField: value2, $name: test-2000,
+  // $createTime: 2025-06-29T11:39:30.642846Z, $updateTime: 2025-06-29T11:39:30.642846Z
+  // }`
+
+  await fsServiceLib.db.updateDocument(
+    documentPath: '/test/test-2000',
+    json: {
+      'doubleField': 9876.5432,
+    },
+  );
+
+  test2000Doc =
+      await fsServiceLib.db.getDocument(documentPath: '/test/test-2000');
+  stdout.writeln('test2000Doc=`$test2000Doc`'); // test2000Doc=`{
+  // doubleField: 9876.5432, $name: test-2000,
+  // $createTime: 2025-06-29T11:39:30.642846Z, $updateTime: 2025-06-29T11:53:19.045956Z
   // }`
 }
